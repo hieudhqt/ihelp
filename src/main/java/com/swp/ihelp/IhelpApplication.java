6afab98f1ca065e0a809d4b08f9ffc30925ed4c5
hieudhqt <hieudhqt@gmail.com>
2021-01-08 15:30:13 +0700

    Initial commit

---
+++ src/main/java/com/swp/ihelp/IhelpApplication.java
@@ -0,0 +1,13 @@
+package com.swp.ihelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IhelpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swp/ihelp/IhelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp.ihelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IhelpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
