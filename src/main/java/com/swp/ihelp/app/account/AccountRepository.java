be6faee6080a00935537cf8fe410692129555b3d
KhoaBui <khoabui.79.99@gmail.com>
2021-04-26 19:49:03 +0700

    - Add API to get top contributor by quarter of year.

--- src/main/java/com/swp/ihelp/app/account/AccountRepository.java
+++ src/main/java/com/swp/ihelp/app/account/AccountRepository.java
@@ -2,6 +2,8 @@ package com.swp.ihelp.app.account;
 
 import com.swp.ihelp.app.account.response.NotEvaluatedParticipantsMapping;
 import com.swp.ihelp.app.account.response.ParticipantsMapping;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -66,6 +68,17 @@ public interface AccountRepository extends JpaRepository<AccountEntity, String> 
             "ORDER BY e.total desc " +
             "LIMIT 0, 100 ", nativeQuery = true)
     List<AccountEntity> getTop100AccountsByContributionAndDate(String startDate, String endDate) throws Exception;
+
+    @Query(value = "SELECT a.* " +
+            "FROM ihelp.account a  " +
+            "INNER JOIN (SELECT r.account_email, SUM(r.point) as total " +
+            "FROM ihelp.reward r  " +
+            "WHERE DATE(r.created_date) >= :startDate AND DATE(r.created_date) <= :endDate " +
+            "GROUP BY r.account_email) e " +
+            "ON e.account_email = a.email  " +
+            "WHERE a.role_id = 'user' " +
+            "ORDER BY e.total desc", nativeQuery = true)
+    Page<AccountEntity> getTopAccountsByContributionAndDateWithPaging(String startDate, String endDate, Pageable pageable) throws Exception;
 
     @Query(value = "SELECT a FROM AccountEntity a WHERE a.role.name = :roleName")
     List<AccountEntity> findByRoleName(String roleName) throws Exception;
